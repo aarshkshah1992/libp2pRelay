9939ebea6bfd686de75b8e0d008b3afdc549be61
aarshkshah1992 <aarshkshah1992@gmail.com>
2021-02-02 13:52:22 +0530

    create a public IP

--- relayServer.go
+++ relayServer.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
 
 	"github.com/libp2p/go-libp2p"
 	circuit "github.com/libp2p/go-libp2p-circuit"
@@ -12,9 +13,21 @@ import (
 )
 
 func main() {
+	publicIP := os.Getenv("RELAY_IP")
+	factory := func(addrs []ma.Multiaddr) []ma.Multiaddr {
+		if len(publicIP) != 0 {
+			tcp := fmt.Sprintf("/ip4/%s/tcp/12001", publicIP)
+			quic := fmt.Sprintf("/ip4/%s/udp/12001/quic", publicIP)
+			return append(addrs, ma.StringCast(tcp), ma.StringCast(quic))
+		}
+
+		return addrs
+	}
+
 	// A public relay server that supports TCP & QUIC and listens on port 12001
 	ctx := context.Background()
 	h1, err := libp2p.New(ctx, libp2p.ForceReachabilityPublic(), libp2p.EnableRelay(circuit.OptHop),
+		libp2p.AddrsFactory(factory),
 		libp2p.Transport(tcp.NewTCPTransport),
 		libp2p.Transport(quic.NewTransport),
 		libp2p.ListenAddrs(ma.StringCast("/ip4/0.0.0.0/tcp/12001"),
